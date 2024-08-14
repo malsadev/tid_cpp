e7c6121d2b3c702451e460e486feb7d57d2ebca8
malsa080 <malsa080@uottawa.ca>
2024-08-14 15:23:14 +0300

    implement method decl indexation

--- include/indexer.h
+++ include/indexer.h
@@ -2,10 +2,9 @@
 #define INDEXER_H
 #include "clang-c/Index.h"
 
-#include <string_view>
+#include <string>
 #include <unordered_map>
 #include <vector>
-//#include <string>
 
 
 namespace indexer
@@ -14,30 +13,22 @@ namespace indexer
 struct indexer
 {
     static inline bool m_verbose;
-    static inline std::vector<std::string_view> m_clang_options;
+    static inline std::vector<const char*> m_clang_options;
 
     //internal index used by libclang
     static inline CXIndex m_index;
 
     //internal index of directory representation that is *.h files aware
-    static inline std::unordered_map<std::string_view, std::vector<std::string>> working_dir_repr{};
+    static inline std::unordered_map<std::string, std::vector<std::string>> working_dir_repr{};
 
-    static inline std::unordered_map<std::string_view, std::vector<std::string>> external_calls{};
-//    static inline std::unordered_map<std::string_view, std::vector<std::string>> method_decls{};
+    static inline std::unordered_map<std::string, std::vector<std::string>> external_calls{};
+    static inline std::unordered_map<std::string, std::vector<std::string>> method_decls{};
 
 
     static int index(std::string_view file_path);
 };
 
 }
-
-
-
-
-
-
-
-
 
 #endif // INDEXER_H
 
--- indexer.cpp
+++ indexer.cpp
@@ -9,7 +9,7 @@
 #include <string>
 #include <iostream> //need to have autoinclude plugin in codeblocks
 
-namespace   //why are empty namespaces used
+namespace indexer   //why are empty namespaces used
 {
 struct CXClientDataWrapper
 {
@@ -19,7 +19,7 @@ struct CXClientDataWrapper
 
 //TODO: suppress unused parameter warnings for this function
 void inclusion_visitor(CXFile included_file,
-                       CXSourceLocation* inclusionStack,
+                       [[maybe_unused]]CXSourceLocation* inclusionStack,
                        unsigned include_len,
                        CXClientData client_data)
 {
@@ -50,13 +50,13 @@ void inclusion_visitor(CXFile included_file,
     clang_disposeString(included_CXFilename); //Disposing CString
 }
 
-void index_external_calls_helper(std::unordered_map<std::string_view, std::vector<std::string>>& external_calls_map,
+void index_external_calls_helper(std::unordered_map<std::string, std::vector<std::string>>& external_calls_map,
                                  const std::string_view entry_filename,
                                  std::unordered_map<std::string, std::unordered_set<CXCursorKind>>& token_map)
 {
 
     std::vector<std::string> external_method_calls{};
-    external_calls_map[entry_filename] = external_method_calls;
+    external_calls_map[std::string(entry_filename)] = external_method_calls;
     for (auto token_type_pair : token_map)
     {
 
@@ -69,25 +69,139 @@ void index_external_calls_helper(std::unordered_map<std::string_view, std::vecto
                 && type_set.find(CXCursor_UnexposedExpr) != type_set.end()
            )
         {
-            external_calls_map[entry_filename].push_back(cursor_spelling);
+            external_calls_map[std::string(entry_filename)].push_back(cursor_spelling);
         }
 
     }
 
 
 }
+
+
+CXChildVisitResult cursor_visitor(CXCursor current_cursor,[[maybe_unused]] CXCursor parent, CXClientData client_data)
+{
+//do not expand includes, ignore cursors in include for now
+    if (clang_Location_isFromMainFile(clang_getCursorLocation(current_cursor)) == 0)
+    {
+        return CXChildVisit_Continue;
+    }
+
+
+
+
+    CXCursorKind cursor_kind = clang_getCursorKind(current_cursor);
+    CXString kind_CXSpelling = clang_getCursorKindSpelling(cursor_kind);
+    std::string kind_spelling{clang_getCString(kind_CXSpelling)};
+    std::cout << "Cursor Kind: " << kind_spelling << std::endl;
+    clang_disposeString(kind_CXSpelling);
+
+    CXString cursor_CXSpelling = clang_getCursorSpelling(current_cursor);
+    std::string cursor_spelling{clang_getCString(cursor_CXSpelling)};
+    std::cout << "Cursor Spelling: " << cursor_spelling << std::endl;
+    clang_disposeString(cursor_CXSpelling);
+
+//        if (cursor_spelling.empty()) {
+//             return CXChildVisit_Continue;
+//        }
+
+
+    std::unordered_map<std::string, std::unordered_set<CXCursorKind>>* token_map = (std::unordered_map<std::string,
+            std::unordered_set<CXCursorKind>>*) client_data;
+
+    if (token_map ->  find(cursor_spelling) == token_map -> end())   //cursor spelling doesn't exist
+    {
+        std::unordered_set<CXCursorKind> types_set{cursor_kind};
+        token_map -> insert({cursor_spelling, types_set});
+    }
+    else
+    {
+        (token_map -> at(cursor_spelling)).insert(cursor_kind);
+    }
+
+    std::cout << "\n";
+    return CXChildVisit_Recurse;
 }
 
-namespace indexer
+void index_method_decls_helper(std::unordered_map<std::string, std::vector<std::string>>& method_decls,
+                               std::string_view file_path,
+                               std::unordered_map<std::string, std::unordered_set<CXCursorKind>>& token_map)
+{
+//     std::vector<std::string> external_method_calls{};
+//    external_calls_map[entry_filename] = external_method_calls;;
+    for (auto token_type_pair : token_map)
+    {
+
+        std::string cursor_spelling = std::get<0>(token_type_pair);
+        std::unordered_set<CXCursorKind> type_set = std::get<1>(token_type_pair);
+
+
+        if (type_set.find(CXCursor_FunctionDecl) != type_set.end()
+           )
+        {
+            method_decls[std::string(file_path)].push_back(cursor_spelling);
+        }
+
+    }
+
+}
+
+
+int index_method_decls(std::string_view file_path, std::unordered_map<std::string, std::vector<std::string>>& method_decls, CXIndex index)
+{
+    std::cerr << "indexing external methods calls in: " << file_path << std::endl;
+
+    CXTranslationUnit unit = clang_parseTranslationUnit(
+                                 index,
+                                 std::string(file_path).c_str(),
+                                 indexer::m_clang_options.data(),
+                                 indexer::m_clang_options.size(),
+                                 nullptr, 0,
+                                 CXTranslationUnit_None); //Parse "file.cpp"
+
+    if (unit == nullptr)
+    {
+        std::cerr << "Unable to parse translation unit. Quitting.\n";
+        return 0;
+    }
+
+//    std::vector<std::string> met; //
+
+    std::unordered_map<std::string, std::unordered_set<CXCursorKind>> token_map{};
+
+    CXCursor cursor = clang_getTranslationUnitCursor(unit);
+    clang_visitChildren(
+        cursor,
+        cursor_visitor,
+        &token_map
+    );
+
+
+    index_method_decls_helper(method_decls, file_path, token_map);
+
+
+    std::cerr << "Completed indexing method declarations in: " << file_path << std::endl;
+    clang_disposeTranslationUnit(unit);
+    return 1;
+
+
+}
+
+
+
+}
+
+namespace indexer //move methods above into this namespace
 {
 
-int index_headers(std::string_view file_path, CXIndex index, std::unordered_map<std::string_view, std::vector<std::string>>& working_dir_repr)
+int index_headers(std::string_view file_path, CXIndex index, std::unordered_map<std::string, std::vector<std::string>>& working_dir_repr)
 {
     std::cerr << "index_headers_called for file: " << file_path << std::endl;
 
     CXTranslationUnit unit = clang_parseTranslationUnit(
                                  index,
-                                 std::string(file_path).c_str(), nullptr, 0,
+                                 std::string(file_path).c_str(),
+                                 indexer::m_clang_options.data(),
+                                 indexer::m_clang_options.size(),
                                  nullptr, 0,
                                  CXTranslationUnit_None); //Parse "file.cpp"
 
@@ -108,7 +222,7 @@ int index_headers(std::string_view file_path, CXIndex index, std::unordered_map<
         &client_data_wrapper //and bring client_wrapper or define it within this file
     );
 
-    working_dir_repr[file_path] = included_headers;
+    working_dir_repr[std::string(file_path)] = included_headers;
 
     for(auto& header: included_headers)
     {
@@ -125,16 +239,18 @@ int index_headers(std::string_view file_path, CXIndex index, std::unordered_map<
 }
 
 int index_external_calls(const std::string_view entry_filename,
-                         CXIndex& index,
-                         std::unordered_map<std::string_view,
+                         CXIndex index,
+                         std::unordered_map<std::string,
                          std::vector<std::string>> &external_calls_map
-                         )
+                        )
 {
     std::cerr << "indexing external methods calls in: " << entry_filename << std::endl;
 
     CXTranslationUnit unit = clang_parseTranslationUnit(
                                  index,
-                                 std::string(entry_filename).c_str(), nullptr, 0,
+                                 std::string(entry_filename).c_str(),
+                                 indexer::m_clang_options.data(),
+                                 indexer::m_clang_options.size(),
                                  nullptr, 0,
                                  CXTranslationUnit_None); //Parse "file.cpp"
 
@@ -151,7 +267,7 @@ int index_external_calls(const std::string_view entry_filename,
     CXCursor cursor = clang_getTranslationUnitCursor(unit);
     clang_visitChildren(
         cursor,
-        [](CXCursor current_cursor, CXCursor parent, CXClientData client_data)
+        [](CXCursor current_cursor, [[maybe_unused]]CXCursor parent, CXClientData client_data)
     {
         //do not expand includes, ignore cursors in include for now
         if (clang_Location_isFromMainFile(clang_getCursorLocation(current_cursor)) == 0)
@@ -207,7 +323,7 @@ int index_external_calls(const std::string_view entry_filename,
 //        if (files_map.find(header) == files_map.end())
 //        {
 //            index_headers(files_map, header, index);
-//        }
+//        }std::unordered_map<std::string, std::vector<std::string>>
 //    }
 
     std::cerr << "Completed indexing external calls in: " << entry_filename << std::endl;
@@ -216,6 +332,29 @@ int index_external_calls(const std::string_view entry_filename,
 
 }
 
+int index_included_method_decls(std::string_view file_path, CXIndex index,
+                                 std::unordered_map<std::string, std::vector<std::string>>& method_decls,
+                                  std::unordered_map<std::string, std::vector<std::string>>& files_map)   //no need to do reference binding for pointer CXIndex, copying pointers is cheap
+{
+
+
+    for (auto& header: files_map[std::string(file_path)])
+    {
+        //TODO: propagate files_map to find method declearations at every level of the include dependancies
+        if (index_method_decls(header, method_decls, index) == 1) //don't propagate files map just yet
+        {
+            std::cout << "Succefully method declarations in header: " << header << std::endl;
+        }
+        else
+        {
+            std::cerr << "Failed to index method declarations in: " << header << std::endl;
+            return 0;
+        }
+    }
+
+    return 1;
+
+}
 
 int indexer::index(std::string_view file_path)
 {
@@ -223,7 +362,7 @@ int indexer::index(std::string_view file_path)
     if (index_headers(file_path, indexer::m_index, indexer::working_dir_repr) == 1)   //succefully travested directory from entry_filename
     {
         //there is a problem with string_view as keys, I don't understand the state of the program
-        std::unordered_map<std::string_view, std::vector<std::string>> tmp = indexer::working_dir_repr;
+        std::unordered_map<std::string, std::vector<std::string>> tmp = indexer::working_dir_repr;
         std::cout << "Succefully indexed headers for filename: " << file_path << std::endl;
     }
     else     //error with indexing headers
@@ -232,19 +371,33 @@ int indexer::index(std::string_view file_path)
         return 0; // fail fast
     }
 
+    //Index external calls recursively
     if(index_external_calls(file_path, indexer::m_index, indexer::external_calls) == 1)  //is it possible to reuse translation unit in previous file? probably should cache translation units
     {
-         std::cout << "Succefully indexed headers for filename: " << file_path << std::endl;
+        std::unordered_map<std::string, std::vector<std::string>> tmp = indexer::external_calls;
+        std::cout << "Succefully indexed external calls for filename: " << file_path << std::endl;
 
-    } else
+    }
+    else
     {
-        std::cerr << "Failed to index headers for filename: " << file_path << std::endl;
+        std::cerr << "Failed to index external calls for filename: " << file_path << std::endl;
         return 0; // fail fast
     }
 
-    indexer::working_dir_repr;
-    indexer::external_calls;
+    //Index included method decls recursively
+    if(index_included_method_decls(file_path, indexer::m_index, indexer::method_decls, indexer::working_dir_repr) == 1)
+    {
+        std::unordered_map<std::string, std::vector<std::string>> tmp = indexer::method_decls;
+        std::cout << "Succefully indexed included method declarations for filename: " << file_path << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed to index included method decls for filename: " << file_path << std::endl;
+        return 0; // fail fast
+    }
 
+
+    return 1;
 
 }
 }
